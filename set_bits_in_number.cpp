20c709de12a33921f431abb1fb08307074981c5f
Sameer Kirwale <109839366+sameerkirwale@users.noreply.github.com>
2022-10-03 16:59:43 +0530

    Set bits in a number
    It returns the number of set bits ( 1 bits ) in a number.

---
+++ set_bits_in_number.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std ;
+
+// no. of set bits in a  number
+
+int main () {
+	
+	int input_number, set_bit = 0 ; 
+	int total_bits = 32 ;
+	
+	cout << "Enter the number you want : ";
+	cin >> input_number ;
+	
+	while ( total_bits > 0 ) {
+		
+		if ( input_number & 1 == 1 ) { // this returns 1 if lsb of number is 1, otherwise it returns zero
+			set_bit ++ ; 
+		}
+		else {
+			set_bit += 0 ;
+		}
+		
+		input_number = input_number >> 1 ; // right shifting the number by 1 bit
+		total_bits -- ;
+		
+	}
+	
+	cout << "No. of 1's in the given number is / are : "<< set_bit << endl;
+}
