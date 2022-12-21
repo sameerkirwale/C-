1e45aa336bc29127cc5ba2bc6d1fb25484c7dc88
Sameer Kirwale <109839366+sameerkirwale@users.noreply.github.com>
2022-12-21 13:09:32 +0530

    Convert a decimal number into binary form

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main () {
+	
+	int input_num, binary_num = 0, i = 0, digit ;
+	
+	cin >> input_num ;
+	
+	while ( input_num > 0 ) {
+		
+		digit = input_num & 1 ; // get the digit
+		
+		binary_num += ( pow ( 10, i ) * digit ) ; // covert the digit to binary form
+		
+		input_num = input_num >> 1 ; // right shift the number
+		
+		i++ ;
+	}
+	
+	cout << "Binary form of given number is :" << binary_num << endl ;
+}
